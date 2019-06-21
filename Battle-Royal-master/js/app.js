d69e5969f6c506b65bddc9cdedb21328effc79ce
Chris Jones <ipv6freely@gmail.com>
2019-06-20 17:35:56 -0700

    Add files via upload

---
+++ Battle-Royal-master/js/app.js
@@ -0,0 +1,71 @@
+var highscore = 0;
+
+;var Game = (function() {
+
+    var instance;
+
+
+
+    function init(){
+        return {
+            generateEnemies: function(level){
+                var enemyList = []
+                for(i=0; i < level; i++){
+                    var xCoord = Math.floor(Math.abs(Math.random() * ctx.canvas.width - 30)), 
+                        yCoord = Math.floor(Math.abs(Math.random() * ctx.canvas.height - 30)),
+                        speed = Math.floor(Math.abs(Math.random() * 30 + (5*level)))
+                    ;
+                    enemyList.push(new Enemy(xCoord, yCoord, speed));
+                }
+                return enemyList;
+            },
+
+            running: true,
+            level: 1,
+            allEnemies: [],
+            player: new Player(250, 250),
+            lives:3,
+            gameMap : [
+                [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
+                [1,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+                [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+                [1,0,0,0,0,0,0,0,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+                [1,0,0,0,0,0,0,0,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+                [1,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+                [1,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+                [1,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+                [1,0,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1],
+                [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1],
+                [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
+            ]
+        }
+    }
+
+    return {
+        getInstance: function(){
+            if(!instance){
+                instance = init();
+            }
+            return instance;
+        }
+    };
+
+})();
+
+var game = Game.getInstance();
+game.allEnemies = game.generateEnemies(game.level);
+
+
+document.addEventListener('keyup', function(e) {
+    var allowedKeys = {
+        32: 'space',
+        37: 'left',
+        38: 'up',
+        39: 'right',
+        40: 'down',
+    };
+    game.player.handleInput(allowedKeys[e.keyCode]);
+
+});
+
+
